698c785a93c987ad1729e9dda41d0d999ba31319
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2023-01-08 17:30:02 +0000

    Add SAC to experiment

--- gym_exps/configs.py
+++ gym_exps/configs.py
@@ -3,9 +3,9 @@ import torch.optim as optim
 from hydra.core.config_store import ConfigStore
 from hydra_zen import MISSING, builds, make_config, make_custom_builds_fn
 
-from deeprl.actor_critic_methods import TD3
+from deeprl.actor_critic_methods import SAC, TD3
 from deeprl.actor_critic_methods.experience_replay import UER
-from deeprl.actor_critic_methods.neural_network.mlp import Actor, Critic
+from deeprl.actor_critic_methods.neural_network import mlp
 from deeprl.actor_critic_methods.noise_injection.action_space import Gaussian
 
 pbuilds = make_custom_builds_fn(zen_partial=True, populate_full_signature=True)
@@ -31,28 +31,41 @@ Walker2d = builds(gymnasium.make, id="Walker2d-v4")
 cs.store(group="env", name="Walker2d", node=Walker2d)
 
 
+SACConf = builds(
+    SAC,
+    device=MISSING,
+    state_dim=MISSING,
+    action_dim=MISSING,
+    policy=pbuilds(mlp.GaussianPolicy, hidden_dims=[256, 256]),
+    critic=pbuilds(mlp.ActionValue, hidden_dims=[256, 256]),
+    policy_optimiser=pbuilds(optim.Adam, lr=3e-4),
+    critic_optimiser=pbuilds(optim.Adam, lr=3e-4),
+    temperature_optimiser=pbuilds(optim.Adam, lr=3e-4),
+    experience_replay=builds(UER, capacity=1_000_000),
+    batch_size=256,
+    discount_factor=0.99,
+    target_smoothing_factor=5e-3,
+)
+cs.store(group="algo", name="SAC", node=SACConf)
+
 TD3Conf = builds(
     TD3,
     device=MISSING,
     state_dim=MISSING,
     action_dim=MISSING,
-    policy="${policy}",
-    critic=pbuilds(Critic, hidden_dims=[400, 300], activation_func='relu'),
+    policy=pbuilds(mlp.Policy, hidden_dims=[400, 300]),
+    critic=pbuilds(mlp.ActionValue, hidden_dims=[400, 300]),
     policy_optimiser=pbuilds(optim.Adam, lr=3e-4),
     critic_optimiser=pbuilds(optim.Adam, lr=3e-4),
     experience_replay=builds(UER, capacity=1_000_000),
     batch_size=2**10,
     discount_factor=0.99,
-    polyak=0.99,
+    target_smoothing_factor=5e-3,
     policy_noise=builds(Gaussian, stddev=0.1),
     smoothing_noise_stddev=0.2,
     smoothing_noise_clip=0.5,
 )
 cs.store(group="algo", name="TD3", node=TD3Conf)
-
-
-ANN = pbuilds(Actor, hidden_dims=[400, 300], activation_func='relu', output_func='tanh')
-cs.store(group="policy", name="ANN", node=ANN)
 
 
 ExperimentConf = make_config(
@@ -64,6 +77,5 @@ ExperimentConf = make_config(
     ],
     env=MISSING,
     algo=MISSING,
-    policy=MISSING,
-    num_episodes=100_000,
+    num_episodes=1_000_000,
 )
--- gym_exps/launch.py
+++ gym_exps/launch.py
@@ -17,12 +17,11 @@ jobs = launch(
         "hydra.sweep.subdir=${hydra.job.override_dirname}",
         "hydra.job_logging.handlers.file.filename=${hydra.runtime.output_dir}/${hydra.job.name}.log",
         # Configure ray.init() and ray.remote()
-        "+hydra.launcher.ray.remote.num_gpus=0.3",  # https://docs.ray.io/en/latest/ray-core/tasks/using-ray-with-gpus.html#fractional-gpus
+        "+hydra.launcher.ray.remote.num_gpus=0.2",  # https://docs.ray.io/en/latest/ray-core/tasks/using-ray-with-gpus.html#fractional-gpus
         "+hydra.launcher.ray.remote.max_calls=1",  # forces GPU tasks to release resources after finishing
         # Configure Experiments
         "+env=Ant,HalfCheetah,Hopper,Humanoid,Walker2d",
-        "+algo=TD3",
-        "+policy=ANN",
+        "+algo=SAC,TD3",
     ],
     multirun=True,
     version_base=None,  # FIXME: The version_base parameter is not specified. Please specify a compatability version level, or None. Will assume defaults for version 1.1
