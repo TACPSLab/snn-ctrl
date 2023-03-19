bcb2fd64fe630657ef13539560e61c20c12d247f
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2023-03-19 23:32:49 +0000

    Implement mapless navi env but it has critical issues

---
+++ env.py
@@ -0,0 +1,137 @@
+from math import pi
+from time import sleep
+
+import torch
+from attrs import define, field
+from rospy import ServiceProxy, Publisher, init_node, wait_for_service, wait_for_message
+from roslaunch import configure_logging
+from roslaunch.parent import ROSLaunchParent
+from roslaunch.rlutil import get_or_generate_uuid
+from rospkg import RosPack
+from gazebo_msgs.srv import SpawnModel, DeleteModel, GetModelState, SetModelState
+from gazebo_msgs.msg import ModelState
+from geometry_msgs.msg import Pose, Point, Quaternion, Twist, Vector3
+from sensor_msgs.msg import LaserScan
+from std_srvs.srv import Empty
+from typing import Any, Dict, Optional, Tuple
+from torch import Tensor
+
+
+@define
+class Env:
+    """
+    Assume Gazebo service connections are stable.
+
+    FIXME Shutting down gracefully
+        Tried __del__ but does not work.
+
+    FIXME https://github.com/ros-simulation/gazebo_ros_pkgs/issues/864
+
+    FIXME Know if a model has been spawned before setting its state
+        https://answers.ros.org/question/266043/know-if-a-model-exists-in-my-gazebo-simulation/
+
+    FIXME Allow robot and goal penetrate into each other
+        https://answers.gazebosim.org//question/20191/gazebo-not-updating-visual-position-in-gzclient/
+
+    TODO Timing
+        - Gazebo Classic step sim
+        - ROS clock?
+    """
+
+    _device: torch.device
+    _uuid: Any = get_or_generate_uuid(None, False)
+    _reset_simulation: ServiceProxy = ServiceProxy("/gazebo/reset_simulation", Empty)
+    _get_model_state: ServiceProxy = ServiceProxy("/gazebo/get_model_state", GetModelState)
+    _set_model_state: ServiceProxy = ServiceProxy("/gazebo/set_model_state", SetModelState)
+    _cmd_vel: Publisher = Publisher("/cmd_vel", Twist, queue_size=1)
+    _launch: ROSLaunchParent = field(init=False)
+
+    def __attrs_post_init__(self) -> None:
+        configure_logging(self._uuid)  # Scripts using roslaunch MUST call configure_logging
+        self._launch = ROSLaunchParent(
+            self._uuid,
+            [RosPack().get_path("mapless_navi") + "/launch/main.launch"],
+        )
+        self._launch.start()
+
+        init_node("env")
+        wait_for_service("/gazebo/reset_simulation")
+        wait_for_service("/gazebo/get_model_state")
+        wait_for_service("/gazebo/set_model_state")
+
+    def step(self, action: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor, Dict[str, Any]]:
+        self._cmd_vel.publish(
+            linear  = Vector3(*action[0:3]),
+            angular = Vector3(*action[3:6]),
+        )
+
+        # FIXME: https://github.com/ros-simulation/gazebo_ros_pkgs/issues/1268
+        sleep(0.1)
+
+        obs = self._get_obs()
+        reward = torch.zeros(1, device=self._device, dtype=torch.float32)
+        terminated = torch.tensor([False], device=self._device, dtype=torch.bool)
+        truncated = torch.tensor([False], device=self._device, dtype=torch.bool)
+
+        scan = obs[:36]
+        # <x, y, z> vector that starts at the robot frame origin, terminates at the goal frame origin
+        separation = obs[36:]
+
+        # Collide
+        if scan.min() < 0.2:
+            print("Collide")
+            terminated = torch.tensor([True], device=self._device, dtype=torch.bool)
+            reward -= 500
+
+        distance = separation.norm(p="fro")
+        if distance < 0.3:
+            print("Goal!")
+            terminated = torch.tensor([True], device=self._device, dtype=torch.bool)
+            reward += 1_000
+            # TODO: Reward if stop at the goal. Punish if rush past it.
+
+        # Angle between the vector and x-axis
+        relative_bearing = torch.arccos(separation[0] / distance)
+        reward += 10 * (2*pi - relative_bearing)  # TODO: Expose the factor
+
+        # TODO: Truncate if standstill
+
+        return obs, reward, terminated, truncated, {}
+
+    def reset(self, *, seed: Optional[int] = None, options: Optional[Dict[str, Any]] = None) -> Tuple[Tensor, Dict[str, Any]]:
+        self._reset_simulation()
+
+        # TODO: Random geometry points
+        self._set_model_state(ModelState(
+            model_name="goal",
+            reference_frame="world",
+            pose=Pose(
+                position=Point(1, 1, 0),
+                orientation=Quaternion(0, 0, 0, 0),
+            ),
+            twist=Twist(
+                Vector3(0, 0, 0),
+                Vector3(0, 0, 0),
+            ),
+        ))
+
+        return self._get_obs(), {}
+
+    def close(self) -> None:
+        self._launch.shutdown()
+
+    def _get_obs(self) -> Tensor:
+        scan: LaserScan = wait_for_message("/scan", LaserScan)
+        separation: ModelState = self._get_model_state(
+            model_name="goal",
+            relative_entity_name="turtlebot3_waffle_pi",
+        )
+
+        obs = torch.tensor((
+            *scan.ranges[::10],  # Reduce samples size from 360 to 36
+            separation.pose.position.x,
+            separation.pose.position.y,
+            separation.pose.position.z,
+        ), dtype=torch.float32, device=self._device)
+
+        return torch.nan_to_num(obs, posinf=3.5)
---
+++ train.py
@@ -0,0 +1,53 @@
+import atexit
+from functools import partial
+
+import torch
+import torch.optim as optim
+import wandb
+
+from deeprl.actor_critic_methods import SAC
+from deeprl.actor_critic_methods.neural_network import mlp
+from deeprl.actor_critic_methods.experience_replay import UER
+
+from env import Env
+
+
+cuda = torch.device("cuda:1")
+env = Env(device=cuda)
+atexit.register(env.close)
+obs_dim = 36 + 3
+action_dim = 6
+# TODO: Use Optuna for hyperparameter optimisation
+agent = SAC.init(
+    mlp.TanhGaussianPolicy.init(obs_dim, action_dim, [256, 256]),
+    mlp.Quality.init(obs_dim, action_dim, [256, 256]),
+    partial(optim.Adam, lr=3e-4),
+    partial(optim.Adam, lr=3e-4),
+    partial(optim.Adam, lr=3e-4),
+    UER(1_000_000),
+    256,
+    0.99,
+    -action_dim,
+    5e-3,
+    device=cuda,
+)
+run = wandb.init(project="MaplessNavi")
+
+obs, _ = env.reset(seed=None); episodic_return = 0
+for step in range(1_000_000):
+    action = agent.compute_action(obs)
+    next_obs, reward, terminated, truncated, _ = env.step(action)
+    agent.step(obs, action, reward, next_obs, terminated)
+    episodic_return += reward
+
+    if not terminated and not truncated:
+        obs = next_obs
+    else:
+        run.log({
+            "episodic_return": episodic_return,
+        })
+        obs, _ = env.reset(); episodic_return = 0
+
+    # TODO: Save checkpoints into wandb local dir for syncing
+    if step % 50 == 0:
+        torch.onnx.export(agent._policy.eval(), obs, "policy.onnx"); agent._policy.train()
