88e3396a5b804099c54207778fe8d6dd834e2774
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2023-03-21 02:13:51 +0000

    Add vistualisation of trained policy

---
+++ vis.py
@@ -0,0 +1,26 @@
+import atexit
+
+import torch  # https://github.com/microsoft/onnxruntime/issues/11092#issuecomment-1386840174
+import onnxruntime as ort
+
+from env import Env
+
+providers = [
+    ('CUDAExecutionProvider', {
+        'device_id': 1,
+    }),
+    'CPUExecutionProvider',
+]
+policy = ort.InferenceSession("policy.onnx", providers=providers)
+env = Env(device=torch.device("cpu"))
+atexit.register(env.close)
+
+obs, _ = env.reset(seed=None)
+for step in range(1000):
+    action = policy.run(None, {"onnx::MatMul_0": obs.numpy()})
+    next_obs, reward, terminated, truncated, _ = env.step(action[0])
+
+    if not terminated and not truncated:
+        obs = next_obs
+    else:
+        obs, _ = env.reset()
